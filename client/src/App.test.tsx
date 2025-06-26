5db63db19aa5f021d5ec8412bac4ba370935c148
FrankFMY <pryanishnikovartem@gmail.com>
2025-06-26 20:22:20 +0400

    fix(test): актуализирован тест App — теперь проверяет форму входа

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('рендерится форма входа', () => {
+    render(<App />);
+    expect(screen.getByText(/вход/i)).toBeInTheDocument();
 });
+
