7c3e94ec4398e05165e9b7abcea8ce841bdb60fb
FrankFMY <pryanishnikovartem@gmail.com>
2025-06-26 19:53:38 +0400

    Fix: close mongoose connection after tests to remove Jest warning

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -11,7 +11,7 @@ beforeAll(async () => {
 
 afterAll(async () => {
     await mongoose.connection.db.dropDatabase();
-    await mongoose.disconnect();
+    await mongoose.connection.close();
 });
 
 describe('Аутентификация', () => {
--- tests/todo.test.js
+++ tests/todo.test.js
@@ -13,7 +13,7 @@ beforeAll(async () => {
 
 afterAll(async () => {
     await mongoose.connection.db.dropDatabase();
-    await mongoose.disconnect();
+    await mongoose.connection.close();
 });
 
 describe('CRUD задач', () => {
